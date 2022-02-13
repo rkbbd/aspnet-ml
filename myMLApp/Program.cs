5a7f377fe2990510d8f251fc46039a81b185c1eb
md rakib hasan <rakib434@gmail.com>
2022-02-13 19:22:38 +0600

    init

--- myMLApp/Program.cs
+++ myMLApp/Program.cs
@@ -1,17 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using MyMLApp;
 // Add input data
-Console.WriteLine("Write review :");
-string myReview = Console.ReadLine();
-var sampleData = new SentimentModel.ModelInput()
+bool continueCheck = true;
+while (continueCheck)
 {
-    Col0 = myReview
-};
+    Console.WriteLine("Write review :");
+    string myReview = Console.ReadLine();
+    var sampleData = new SentimentModel.ModelInput()
+    {
+        Col0 = myReview
+    };
 
-// Load model and predict output of sample data
-var result = SentimentModel.Predict(sampleData);
+    // Load model and predict output of sample data
+    var result = SentimentModel.Predict(sampleData);
 
-// If Prediction is 1, sentiment is "Positive"; otherwise, sentiment is "Negative"
-string sentiment = result.Prediction == 1 ? "Positive" : "Negative";
-Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
+    // If Prediction is 1, sentiment is "Positive"; otherwise, sentiment is "Negative"
+    string sentiment = result.Prediction == 1 ? "Positive" : "Negative";
+    Console.WriteLine($"Sentiment: {sentiment}\n");
+    Console.WriteLine("Type c for exists....");
+    continueCheck = Console.ReadLine() == "c"? false: true;
+}
 
